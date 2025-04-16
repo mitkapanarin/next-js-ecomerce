6c3e1d1f6d7aef12bbd3fde830ccd78e75f2ed8f
mitkapanarin <mitka_j@yahoo.com>
2025-04-16 13:35:52 +0200

    .

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,13 +1,15 @@
 "use client";
-import { sampleProducts } from "../../data/products";
-import ProductCard from "../../components/ProductCard";
 
-interface CategoryPageParams {
+import ProductCard from "@/app/components/ProductCard";
+import { sampleProducts } from "@/app/data/products";
+import { use } from "react";
+
+interface CategoryPageParamsPromise {
   slug: string;
 }
 
 interface CategoryPageProps {
-  params: CategoryPageParams;
+  params: Promise<CategoryPageParamsPromise>;
 }
 
 const handleAddToCart = (productId: string | number) => {
@@ -15,7 +17,11 @@ const handleAddToCart = (productId: string | number) => {
   alert(`(Placeholder) Added product ID ${productId} to cart!`);
 };
 
-export default function CategoryPage({ params }: CategoryPageProps) {
+export default function CategoryPage({
+  params: paramsPromise,
+}: CategoryPageProps) {
+  // Use the 'use' hook to resolve the params Promise
+  const params = use(paramsPromise);
   const categorySlug = params.slug;
 
   const productsToShow = sampleProducts.filter(
